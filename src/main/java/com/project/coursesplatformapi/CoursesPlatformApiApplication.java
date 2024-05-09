5cad1d3123020fb399156c9b1f76dcf75703398d
Tarik Wesley <tarikwesley@gmail.com>
2024-05-09 19:40:01 -0300

    feat: starting project

---
+++ src/main/java/com/project/coursesplatformapi/CoursesPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.coursesplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesPlatformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesPlatformApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/coursesplatformapi/CoursesPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coursesplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
