84915aadd3b15d21489bf7b4a6f0fdb41c4ad8de
Tarik Wesley <tarikwesley@gmail.com>
2024-05-10 20:35:31 -0300

    feat: add settings mysql and flyway migration

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `role` VARCHAR(50) NOT NULL,
+    `created_at` DATE NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `courses` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `code` VARCHAR(50) NOT NULL,
+    `instructor` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(50) NOT NULL,
+    `created_at` DATE NOT NULL,
+    `inactivated_at` DATE,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `registrations` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `course_id` BIGINT NOT NULL,
+    `registration_at` DATE NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`course_id`) REFERENCES `courses`(`id`)
+);
