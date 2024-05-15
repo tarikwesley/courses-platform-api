a3c5d7f4cd9ded5b59a6e5dc1e034b92fde44571
Tarik Wesley <tarikwesley@gmail.com>
2024-05-15 13:08:04 -0300

    fix: add permission for access swagger

--- src/main/java/com/project/coursesplatformapi/config/SecurityConfig.java
+++ src/main/java/com/project/coursesplatformapi/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
